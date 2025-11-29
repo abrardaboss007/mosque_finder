03d2ea8dbb751a68f898acd3ea76c958d0896886
Abrar Nadeem <abrarnadeem007@gmail.com>
2025-11-29 23:19:18 +0000

    Dashboard

--- pages/5_📊_Dashboard.py
+++ pages/5_📊_Dashboard.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.title("Hey Ed! Dashboard here")
+
+st.markdown("I was planning on adding a dashboard where there would be multiple graphs.")
+st.markdown("One that I had in mind was a choropleth map showing a heat density of the mosques around UK")
+st.markdown("Unfortunately I did not have time to complete it before the deadline")
