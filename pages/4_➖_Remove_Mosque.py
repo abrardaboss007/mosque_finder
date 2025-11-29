dc1536da945f2b96aa2546213bf7668b252a93d7
Abrar Nadeem <abrarnadeem007@gmail.com>
2025-11-29 23:22:37 +0000

    Slight modifications

--- pages/4_➖_Remove_Mosque.py
+++ pages/4_➖_Remove_Mosque.py
@@ -10,7 +10,7 @@ import pgeocode
 st.title("Hey Ed! Remove mosques here")
 st.set_page_config(page_title="View/Search/Filter Mosques")
 
-# Initialize session state DataFrame once
+# Initialise session state DataFrame once
 if 'df4' not in st.session_state:
     try:
         st.session_state.df4 = pd.read_csv("uk_mosques_modified.csv")
